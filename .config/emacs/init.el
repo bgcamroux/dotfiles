32996a6f098d03fb0a121c2766d7d9ea167af290
Brad Camroux <bgcamroux@gmail.com>
2023-05-02 15:24:36 -0600

    adding many configs from SystemCrafters config

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -7,8 +7,11 @@
 (tooltip-mode    -1)                 ; disable tooltips
 (menu-bar-mode   -1)                 ; disable menu bar
 (set-fringe-mode 10)                 ; gives us some breathing room
+(set-default-coding-systems 'utf-8)  ; UTF-8 character encoding
 
 (set-face-attribute 'default nil :font "JetBrainsMonoNL Nerd Font Mono" :height 100)
+(set-frame-parameter (selected-frame) 'alpha '(90 . 90))
+(add-to-list 'default-frame-alist '(alpha . (90 . 90)))
 
 ;; Install elpaca package manager
 (defvar elpaca-installer-version 0.3)
@@ -55,16 +58,16 @@
 ; block until current queue is processed - this will allow use of use-package right away
 (elpaca-wait)
 
-(use-package modus-themes)
-(use-package i3wm-config-mode)
-(use-package rainbow-delimiters)
-(use-package org-superstar)
-(use-package orca)
-(use-package org-d20)
-(use-package latex-extra)
-(use-package latex-table-wizard)
-(use-package mu2tex)
-(use-package auctex)
+(use-package modus-themes)        ; modus-operandi and modus-vivendi
+(use-package i3wm-config-mode)    ; better colours for i3wm config
+(use-package rainbow-delimiters)  ; rainbow parentheses
+(use-package org-superstar)       ; ORG - bullets and more
+(use-package orca)                ; ORG - capture
+(use-package org-d20)             ; ORG - d20 D&D
+(use-package latex-extra)         ; LaTeX
+(use-package latex-table-wizard)  ; LaTeX tables
+(use-package mu2tex)              ; LaTeX
+;(use-package auctex)              ; LaTeX
 
 ; block until queue is processed - allows use of these packages
 (elpaca-wait)
@@ -77,6 +80,212 @@
 (setq modus-themes-to-toggle '(modus-operandi-tinted modus-vivendi-tinted))
 (load-theme 'modus-vivendi-tinted t)
 (define-key global-map (kbd "<f5>") #'modus-themes-toggle)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Set up which-key to see what's available based on what prefix has been pressed
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+(elpaca-wait)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Line Numbering
+(column-number-mode)
+
+;; Enable line numbers for some modes
+(dolist (mode '(text-mode-hook
+		prog-mode-hook
+		conf-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 1))))
+
+;; Override some modes deriving from the above
+(dolist (mode '(org-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; From David Wilson of System Crafters
+(defun bgc/replace-unicode-font-mapping (block-name old-font new-font)
+  (let* ((block-idx (cl-position-if
+                         (lambda (i) (string-equal (car i) block-name))
+                         unicode-fonts-block-font-mapping))
+         (block-fonts (cadr (nth block-idx unicode-fonts-block-font-mapping)))
+         (updated-block (cl-substitute new-font old-font block-fonts :test 'string-equal)))
+    (setf (cdr (nth block-idx unicode-fonts-block-font-mapping))
+          `(,updated-block))))
+
+(use-package unicode-fonts
+  :disabled
+  :custom
+  (unicode-fonts-skip-font-groups '(low-quality-glyphs))
+  :config
+  ;; Fix the font mappings to use the right emoji font
+  (mapcar
+    (lambda (block-name)
+      (bgc/replace-unicode-font-mapping block-name "Apple Color Emoji" "Noto Color Emoji"))
+    '("Dingbats"
+      "Emoticons"
+      "Miscellaneous Symbols and Pictographs"
+      "Transport and Map Symbols"))
+  (unicode-fonts-setup)
+  )
+
+(elpaca-wait)
+
+;; Emoji in buffers
+(use-package emojify
+  :hook (erc-mode . emojify-mode)
+  :commands emojify-mode)
+
+(elpaca-wait)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Auto-save changed files
+(use-package super-save
+  :defer 1
+  :diminish super-save-mode
+  :config
+  (super-save-mode +1)
+  (setq super-save-auto-save-when-idle t))
+
+(elpaca-wait)
+
+;; Auto-revert changed files
+(setq global-auto-revert-non-file-buffers t) ; revert dired & other buffers
+(global-auto-revert-mode 1)                  ; revert buffers when underlying file is changed
+
+;; Prompt for the pin of GPG private keys
+(setq epa-pinentry-mode 'loopback)
+;(pinentry-start)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; TRAMP
+(setq tramp-default-method "ssh")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq-default tab-width          2)     ; default to indentation size of 2 spaces
+(setq-default indent-tabs-mode nil)     ; use spaces instead of tabs for indentation
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Completions -- Ivy, Counsel, Swiper
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-f" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :init
+  (ivy-mode 1)
+  :config
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-wrap t)
+  (setq ivy-count-format "(%d/%d) ")
+  (setq enable-recursive-minibuffers t)
+  ;; Use different regex strategies per completion command
+  (push '(completion-at-point . ivy--regex-fuzzy) ivy-re-builders-alist) ;; This doesn't seem to work...
+  (push '(swiper . ivy--regex-ignore-order) ivy-re-builders-alist)
+  (push '(counsel-M-x . ivy--regex-ignore-order) ivy-re-builders-alist)
+  ;; Set minibuffer height for different commands
+  (setf (alist-get 'counsel-projectile-ag ivy-height-alist) 15)
+  (setf (alist-get 'counsel-projectile-rg ivy-height-alist) 15)
+  (setf (alist-get 'swiper ivy-height-alist) 15)
+  (setf (alist-get 'counsel-switch-buffer ivy-height-alist) 7))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1)
+  :after counsel
+  :config
+  (setq ivy-format-function #'ivy-format-function-line)
+  (setq ivy-rich-display-transformers-list
+        (plist-put ivy-rich-display-transformers-list
+                   'ivy-switch-buffer
+                   '(:columns
+                     ((ivy-rich-candidate (:width 40))
+                      (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right)); return the buffer indicators
+                      (ivy-rich-switch-buffer-major-mode (:width 12 :face warning))          ; return the major mode info
+                      (ivy-rich-switch-buffer-project (:width 15 :face success))             ; return project name using `projectile'
+                      (ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3))))))  ; return file path relative to project root or `default-directory' if project is nil
+                     :predicate
+                     (lambda (cand)
+                       (if-let ((buffer (get-buffer cand)))
+                           ;; Don't mess with EXWM buffers
+                           (with-current-buffer buffer
+                             (not (derived-mode-p 'exwm-mode)))))))))
+
+(use-package counsel
+  :demand t
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         ;; ("C-M-j" . counsel-switch-buffer)
+         ("C-M-l" . counsel-imenu)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history))
+  :custom
+  (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
+  :config
+  (setq ivy-initial-inputs-alist nil)) ;; Don't start searches with ^
+
+(use-package flx  ;; Improves sorting for fuzzy-matched results
+  :after ivy
+  :defer t
+  :init
+  (setq ivy-flx-limit 10000))
+
+(use-package wgrep)
+
+(use-package ivy-posframe
+  :disabled
+  :custom
+  (ivy-posframe-width      115)
+  (ivy-posframe-min-width  115)
+  (ivy-posframe-height     10)
+  (ivy-posframe-min-height 10)
+  :config
+  (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-frame-center)))
+  (setq ivy-posframe-parameters '((parent-frame . nil)
+                                  (left-fringe . 8)
+                                  (right-fringe . 8)))
+  (ivy-posframe-mode 1))
+
+(use-package prescient
+  :after counsel
+  :config
+  (prescient-persist-mode 1))
+
+(use-package ivy-prescient
+  :after prescient
+  :config
+  (ivy-prescient-mode 1))
+
+(elpaca-wait)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
